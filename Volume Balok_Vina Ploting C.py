d6eb5f9f8fb403cc045a52970de8b581bfbc748f
vinadwii <143859956+vinadwii@users.noreply.github.com>
2023-09-08 21:08:26 +0700

    Add files via upload

---
+++ Volume Balok_Vina Ploting C.py
@@ -0,0 +1,6 @@
+panjang = int(input('Masukkan panjang balok: '))
+lebar = int(input('Masukkan lebar balok: '))
+tinggi = int(input('Masukkan tinggi balok: '))
+
+volume = int(panjang*lebar*tinggi)
+print('volume balok adalah: ', volume)
---
+++ Volume Bola_Vina Ploting C-1.py
@@ -0,0 +1,9 @@
+print ("Menghitung Volume Bola")
+
+r = int(input("Masukkan Jari - Jari = "))
+
+phi = 22/7
+
+volume = 4/3*phi*r*r*r
+
+print ("Volume Bola = ", volume)
